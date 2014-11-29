70ad05307b3778aeca63b6d2648c261e1bdd19fc
MatthewJA <matthew.alger@gmail.com>
2014-11-29 17:04:52 +1100

    Added Angular.js

---
+++ dragonpost/static/js/app.js
@@ -0,0 +1,13 @@
+(function() {
+
+    // Module setup.
+    var app = angular.module("app", []);
+
+    // Module configuration.
+    // We're changing {{}} to {[]} to avoid conflicts with Jinja2.
+    app.config(function($interpolateProvider) {
+        $interpolateProvider.startSymbol('{[');
+        $interpolateProvider.endSymbol(']}');
+    });
+
+}).call()
